6eb995d6c504763ffd1a1f451c3836ca5283bbf8
PapaKoop <30537601+PapaKoop@users.noreply.github.com>
2018-03-16 15:26:09 -0500

    Add geolcation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -6,10 +6,24 @@ describe('generateMessage', () => {
   it('should generate the correct message object', () => {
     var from = 'Papa';
     var text = 'Some message';
-    var createdAt = new Date().getTime();
+    // var createdAt = new Date().getTime();
     var message = generateMessage(from, text);
 
-//    expect(message.createdAt).toBe(createdAt);
+    expect(typeof message.createdAt).toBe('number');
     expect(message).toInclude({from, text});
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Papa';
+    var latitude = 15;
+    var longitude = 15;
+    var url = 'https://www.google.com/maps?q=15,15';
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toInclude({from, url});
+
   });
 });
