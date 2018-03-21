373b09f43d7b7663564b3642d1347de2ca407baa
PapaKoop <30537601+PapaKoop@users.noreply.github.com>
2018-03-21 14:33:59 -0500

    SCROLL TO BOTTOM IS USER IS CLOSE TO BOTTOM

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 var socket = io();
 
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  var totalMessageHeight = clientHeight + scrollTop + newMessageHeight + lastMessageHeight
+
+  if (totalMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+};
+
 socket.on('connect', function () {
   console.log('Connected to local server');
 });
@@ -19,7 +37,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
-
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -32,6 +50,7 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e) {
